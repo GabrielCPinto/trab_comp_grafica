5b2f2bd7a24eeb2519a2d0dd88b336891439bb47
GabrielCPinto <gabrielcostap112@gmail.com>
2024-04-01 18:21:49 -0300

    video 1 fix

--- Assets/Scripts/Container.cs
+++ Assets/Scripts/Container.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
-[RequiredComponent(typeof(MeshFilter))]
-[RequiredComponent(typeof(MeshRenderer))]
-[RequiredComponent(typeof(MeshCollider))]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
 public class Container : MonoBehaviour
 {
-
     public Vector3 containerPosition;
+
+    private MeshData meshData = new MeshData();
+
     private MeshRenderer meshRenderer;
     private MeshFilter meshFilter;
     private MeshCollider meshCollider;
@@ -20,146 +22,151 @@ public class Container : MonoBehaviour
         meshRenderer.sharedMaterial = mat;
         containerPosition = position;
     }
-    
-    private void ConfigureComponents(){
-        meshFilter = GetComponent<MeshFilter>();
-        meshCollider = GetComponent<MeshCollider>();
-        meshRenderer = GetComponent<MeshRenderer>();
-    }
 
     public void GenerateMesh()
+    {
+        meshData.ClearData();
+
+        Vector3 blockPos = new Vector3(8, 8, 8);
+        Voxel block = new Voxel() { ID = 1 };
+
+        int counter = 0;
+        Vector3[] faceVertices = new Vector3[4];
+        Vector2[] faceUVs = new Vector2[4];
+
+        //Iterate over each face direction
+        for (int i = 0; i < 6; i++)
         {
-            meshData.ClearData();
 
-            Vector3 blockPos = new Vector3(8,8,8);
-            Voxel block = new Voxel() { ID = 1 };
+            //Draw this face
 
-            int counter = 0;
-            Vector3[] faceVertices = new Vector3[4];
-            Vector2[] faceUVs = new Vector2[4];
-
-            for (int i = 0; i < 6; i++)
+            //Collect the appropriate vertices from the default vertices and add the block position
+            for (int j = 0; j < 4; j++)
             {
+                faceVertices[j] = voxelVertices[voxelVertexIndex[i, j]] + blockPos;
+                faceUVs[j] = voxelUVs[j];
+            }
 
-                for (int j = 0; j < 4; j++)
-                {
-                    faceVertices[j] = voxelVertices[voxelVertexIndex[i, j]] + blockPos;
-                    faceUVs[j] = voxelUVs[j];
-                }
+            for (int j = 0; j < 6; j++)
+            {
+                meshData.vertices.Add(faceVertices[voxelTris[i, j]]);
+                meshData.UVs.Add(faceUVs[voxelTris[i, j]]);
 
-                for (int j = 0; j < 6; j++)
-                {
-                    meshData.vertices.Add(faceVertices[voxelTris[i, j]]);
-                    meshData.UVs.Add(faceUVs[voxelTris[i, j]]);
+                meshData.triangles.Add(counter++);
 
-                    meshData.triangles.Add(counter++);
-
-                }
             }
         }
-    
+    }
+
     public void UploadMesh()
-        {
-            meshData.UploadMesh();
+    {
+        meshData.UploadMesh();
 
-            if (meshRenderer == null)
-                ConfigureComponents();
+        if (meshRenderer == null)
+            ConfigureComponents();
 
-            meshFilter.mesh = meshData.mesh;
-            if (meshData.vertices.Count > 3)
-                meshCollider.sharedMesh = meshData.mesh;
-        }
-    
+        meshFilter.mesh = meshData.mesh;
+        if (meshData.vertices.Count > 3)
+            meshCollider.sharedMesh = meshData.mesh;
+    }
+
+    private void ConfigureComponents()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshCollider = GetComponent<MeshCollider>();
+    }
+
     public static Voxel emptyVoxel = new Voxel() { ID = 0 };
 
-        #region Mesh Data
-        public struct MeshData
+    #region Mesh Data
+    public struct MeshData
+    {
+        public Mesh mesh;
+        public List<Vector3> vertices;
+        public List<int> triangles;
+        public List<Vector2> UVs;
+
+        public bool Initialized;
+
+        public void ClearData()
         {
-            public Mesh mesh;
-            public List<Vector3> vertices;
-            public List<int> triangles;
-            public List<Vector2> UVs;
-
-            public bool Initialized;
-
-            public void ClearData()
+            if (!Initialized)
             {
-                if (!Initialized)
-                {
-                    vertices = new List<Vector3>();
-                    triangles = new List<int>();
-                    UVs = new List<Vector2>();
+                vertices = new List<Vector3>();
+                triangles = new List<int>();
+                UVs = new List<Vector2>();
 
-                    Initialized = true;
-                    mesh = new Mesh();
-                }
-                else
-                {
-                    vertices.Clear();
-                    triangles.Clear();
-                    UVs.Clear();
-                    mesh.Clear();
-                }
+                Initialized = true;
+                mesh = new Mesh();
             }
-            public void UploadMesh(bool sharedVertices = false)
+            else
             {
-                mesh.SetVertices(vertices);
-                mesh.SetTriangles(triangles, 0, false);
-                mesh.SetUVs(0, UVs);
-
-                mesh.Optimize();
-
-                mesh.RecalculateNormals();
-
-                mesh.RecalculateBounds();
-
-                mesh.UploadMeshData(false);
+                vertices.Clear();
+                triangles.Clear();
+                UVs.Clear();
+                mesh.Clear();
             }
         }
-
-        #endregion
-
-        #region Static Variables
-
-        static readonly Vector3[] voxelVertices = new Vector3[8]
+        public void UploadMesh(bool sharedVertices = false)
         {
-            new Vector3(0,0,0),//0
-            new Vector3(1,0,0),//1
-            new Vector3(0,1,0),//2
-            new Vector3(1,1,0),//3
+            mesh.SetVertices(vertices);
+            mesh.SetTriangles(triangles, 0, false);
+            mesh.SetUVs(0, UVs);
 
-            new Vector3(0,0,1),//4
-            new Vector3(1,0,1),//5
-            new Vector3(0,1,1),//6
-            new Vector3(1,1,1),//7
-        };
+            mesh.Optimize();
 
-        static readonly int[,] voxelVertexIndex = new int[6, 4]
-        {
-            {0,1,2,3},
-            {4,5,6,7},
-            {4,0,6,2},
-            {5,1,7,3},
-            {0,1,4,5},
-            {2,3,6,7},
-        };
+            mesh.RecalculateNormals();
 
-        static readonly Vector2[] voxelUVs = new Vector2[4]
-        {
-            new Vector2(0,0),
-            new Vector2(0,1),
-            new Vector2(1,0),
-            new Vector2(1,1)
-        };
+            mesh.RecalculateBounds();
 
-        static readonly int[,] voxelTris = new int[6, 6]
-        {
-            {0,2,3,0,3,1},
-            {0,1,2,1,3,2},
-            {0,2,3,0,3,1},
-            {0,1,2,1,3,2},
-            {0,1,2,1,3,2},
-            {0,2,3,0,3,1},
-        };
-        #endregion
+            mesh.UploadMeshData(false);
+        }
     }
+
+    #endregion
+
+    #region Static Variables
+
+    static readonly Vector3[] voxelVertices = new Vector3[8]
+    {
+        new Vector3(0,0,0),//0
+        new Vector3(1,0,0),//1
+        new Vector3(0,1,0),//2
+        new Vector3(1,1,0),//3
+
+        new Vector3(0,0,1),//4
+        new Vector3(1,0,1),//5
+        new Vector3(0,1,1),//6
+        new Vector3(1,1,1),//7
+    };
+
+    static readonly int[,] voxelVertexIndex = new int[6, 4]
+    {
+        {0,1,2,3},
+        {4,5,6,7},
+        {4,0,6,2},
+        {5,1,7,3},
+        {0,1,4,5},
+        {2,3,6,7},
+    };
+
+    static readonly Vector2[] voxelUVs = new Vector2[4]
+    {
+        new Vector2(0,0),
+        new Vector2(0,1),
+        new Vector2(1,0),
+        new Vector2(1,1)
+    };
+
+    static readonly int[,] voxelTris = new int[6, 6]
+    {
+        {0,2,3,0,3,1},
+        {0,1,2,1,3,2},
+        {0,2,3,0,3,1},
+        {0,1,2,1,3,2},
+        {0,1,2,1,3,2},
+        {0,2,3,0,3,1},
+    };
+    #endregion
+}
